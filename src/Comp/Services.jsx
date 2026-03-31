d01372a524b8bc875e6ab69dc560973fa024d95b
vishesh singh <visheshpratapsingh12@gmail.comgit config --global user.name>
2026-03-31 14:15:37 +0530

    fixed icon issue

--- src/Comp/Services.jsx
+++ src/Comp/Services.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FaLaptopCode, FaMobileAlt, FaBug, FaServer , FaMagnifyingGlass } from "react-icons/fa";
+import { FaLaptopCode, FaMobileAlt, FaBug, FaServer  } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const services = [
