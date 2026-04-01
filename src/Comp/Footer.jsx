963326eec017315bc938b61450b528c662491657
vishesh singh <visheshpratapsingh12@gmail.comgit config --global user.name>
2026-04-01 08:31:44 +0530

    small changes

--- src/Comp/Footer.jsx
+++ src/Comp/Footer.jsx
@@ -1,7 +1,7 @@
 // import { FaGithub, FaLinkedin, FaFiverr } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { SiFiverr } from "react-icons/si"; // Fiverr icon
+
 
 const Footer = () => {
   return (
@@ -16,9 +16,7 @@ const Footer = () => {
       <div className="flex justify-center gap-4 mb-4 text-xl">
         <a href="https://github.com/Vishesh06-fullstack" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400"><FaGithub /></a>
         <a href="https://www.linkedin.com/in/vishesh-pratap-singh-43ba27211/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400"><FaLinkedin /></a>
-        {/* <a href="https://fiverr.com/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">
-  <SiFiverr />
-</a> */}
+
       </div>
       <p className="text-gray-500 text-sm">© {new Date().getFullYear()} Vishesh. All rights reserved.</p>
     </motion.footer>
